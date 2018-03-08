2b2ac3f2bf1ae24f9a47b2b150c8cc774208568e
gerardramosayasdi <gerard.ramosm89@gmail.com>
2018-03-08 12:30:25 -0800

    finishing adding babel

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -14,6 +14,15 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
         test: /\.(css|scss)$/,
         use: [
           {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 console.log('hello from main.js!');
 document.querySelector('#root').innerHTML = `<h1>Hello from main.js!</h1>`;
 
+const x = () => {
+  console.log("this is an async arrow function");
+}
 require('./main.css');
 require('./main.scss');
 require('./index.html');
